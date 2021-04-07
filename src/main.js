254014a422b1a63608a64f1440208da69287a459
Sr. Darío Milanés <dario.milanes@gmail.com>
2021-04-07 21:28:50 +0100

    Add

--- src/main.js
+++ src/main.js
@@ -12,10 +12,5 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-VanillaTilt.init(document.querySelector(".image"), {
-  max: 25,
-  speed: 400
-});
-
 //It also supports NodeList
 VanillaTilt.init(document.querySelectorAll(".image"));
